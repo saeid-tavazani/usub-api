79e73192fd37072eb06a05bc542884e33c8d6730
saeid tavazani <saeidtavazani@gmail.com>
2023-12-09 09:53:58 -0800

    fix auth

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -19,32 +19,3 @@ exports.auth = (req, res, next) => {
   }
   next();
 };
-
-exports.authAdmin = (req, res, next) => {
-  if (!("authorization" in req.headers)) {
-    return res.status(401).send({
-      status: "error",
-      code: 401,
-      message: "you are not authorized!",
-    });
-  }
-  const token = TokenService.verify(req.headers.authorization);
-  const tokenData = TokenService.decode(req.headers.authorization);
-  if (!token) {
-    return res.status(401).send({
-      status: "error",
-      code: 401,
-      message: "you token is not valid!",
-      success: false,
-    });
-  }
-  if (token && tokenData.role != "admin") {
-    return res.status(401).send({
-      status: "error",
-      code: 401,
-      message: "you do not have permission to access!",
-      success: false,
-    });
-  }
-  next();
-};
