fe081be47b86b5cd3d5692d4f6048401a5747f31
saeid tavazani <saeidtavazani@gmail.com>
2023-12-15 02:28:19 -0800

    fix routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,6 @@ const sessionRouter = require("./sessions");
 const users = require("./users");
 
 module.exports = (app) => {
-  app.use("/app/usab/api/v1/session", sessionRouter);
-  app.use("/app/usab/api/v1/users", users);
+  app.use("/app/usub/api/v1/session", sessionRouter);
+  app.use("/app/usub/api/v1/users", users);
 };
