63adf6b03969ecc74ec0bbb1367c42654e87fc75
saeid tavazani <saeidtavazani@gmail.com>
2024-01-27 04:40:41 -0800

    add transactionModels

---
+++ app/models/transactionModels.ts
@@ -0,0 +1,30 @@
+import { sequelize, DataTypes } from "./index";
+
+const transaction = sequelize.define("transaction", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  amount: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  date: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  description: {
+    type: DataTypes.STRING(11),
+  },
+  model: {
+    type: DataTypes.ENUM("receive", "payment"),
+    defaultValue: "payment",
+  },
+});
+
+export default transaction;
