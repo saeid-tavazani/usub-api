00a0a92035d03f7a35a3cc7fbb456764166c30ba
saeid tavazani <saeidtavazani@gmail.com>
2024-01-27 06:37:03 -0800

    add newContact

---
+++ app/controllers/categoryController.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import errorLogger from "../services/errorLogger";
+import category from "../models/categoryModels";
+import {
+  errorNot,
+  errorRequest,
+  successAdd,
+} from "../services/responseStatusCodes";
+const newContact = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { name, type, userId } = req.body;
+    category
+      .create({
+        title: name,
+        type: type,
+        userId: userId,
+      })
+      .then((response) => {
+        if (response) {
+          res.send(successAdd);
+        } else {
+          res.send(errorNot);
+        }
+      })
+      .catch((error) => {
+        res.send(errorRequest);
+        errorLogger.error(error);
+      });
+  } catch (error) {
+    errorLogger.error(error);
+    next(error);
+  }
+};
+
+export { newContact };
