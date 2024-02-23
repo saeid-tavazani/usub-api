173141677314bab1bd36e4ca9df86554800281a5
saeid tavazani <saeidtavazani@gmail.com>
2024-02-23 05:23:51 -0800

    fix jsonwebtoken

--- app/services/tokenService.ts
+++ app/services/tokenService.ts
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const appSecret: string = "1b8f3a6e9d0c7b2f6e1d3f7c8b3a9d4b2c6e1d3f";
 const sign = (data: object) => {
   if (appSecret) {
-    return jwt.sign(data, appSecret, { expiresIn: "48h" });
+    return jwt.sign(data, appSecret, { expiresIn: "200h" });
   } else {
     throw new Error("APP_SECRET is undefined");
   }
